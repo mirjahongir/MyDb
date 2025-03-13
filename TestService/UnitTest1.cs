5cad71eb0ed0cc23ef6f4d19606023a86669d6c3
murtozayev jahongir <mirjahongirbek@gmail.com>
2025-03-13 17:33:42 +0500

    Add project files.

---
+++ TestService/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using Services.Parser;
+
+namespace TestService
+{
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+
+        }
+        public static string _command = "  create database йоха";
+
+        [Fact]
+        public void Trim()
+        {   // Arrange
+
+            var start = 0;
+            var span = _command.AsSpan();
+            // Act
+            var result = ParseRequest.Trim(ref span, start);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+        [Fact]
+        public void GetFirstChar()
+        {
+            // Arrange
+            var span = _command.AsSpan();// "  create database йоха".AsSpan();
+            var start = 0;
+            // Act
+            var result = ParseRequest.GetFirstChar(ref span, start);
+            Console.WriteLine(result.Item1);
+            Console.WriteLine(result.Item2);
+            // Assert
+        }
+        [Fact]
+        public void GetKeyword()
+        {
+            // Arrange
+            var span = _command.AsSpan();
+            var start = 0;
+            char? key;
+
+            // Act
+            (key, start) = ParseRequest.GetFirstChar(ref span, start);
+            var result = ParseRequest.GetKeyword(ref span, start, key);
+            // Assert
+        }
+        [Fact]
+        public void GetToken()
+        {
+            var span = _command.AsSpan();
+            var start = 0;
+            var token = ParseRequest.ParseToken(ref span, start);
+            
+        }
+    }
+}
---
+++ libs/DbEnums/Enums/Parser/TokenType.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DbEnums.Enums.Parser
+{
+    public enum TokenType
+    {
+        None = 0,
+        Command,
+    }
+    public enum CmdType
+    {
+        NONE = 0,
+        Create,
+    }
+}
